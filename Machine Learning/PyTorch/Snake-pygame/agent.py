1f35e6073eebfef2b36814e09272c8488fa1c226
Alexander Drysdale <drysdalealex0@gmail.com>
2024-09-13 16:44:16 +0100

    get_action method being finalised

--- Machine Learning/PyTorch/Snake-pygame/agent.py
+++ Machine Learning/PyTorch/Snake-pygame/agent.py
@@ -15,6 +15,8 @@ class Agent:
         self.epsilon = 0 # parameter to control the randomness 
         self.gamma = 0 # discount rate 
         self.memory = deque(maxlen=MAX_MEMORY) # if we excedd this memory
+        self.model = None 
+        self.trainer = None
         # TODO: model, trainer
     
     def get_state(self, game):
@@ -44,25 +46,53 @@ class Agent:
             (dir_r and game.is_collision(point_d)),
 
             # Danger left
-            (dir_r and game.is_collision(point_r)) or
-            (dir_l and game.is_collision(point_l)) or
-            (dir_u and game.is_collision(point_u)) or
-            (dir_d and game.is_collision(point_d)),
+            (dir_d and game.is_collision(point_r)) or
+            (dir_u and game.is_collision(point_l)) or
+            (dir_r and game.is_collision(point_u)) or
+            (dir_l and game.is_collision(point_d)),
 
+            # Moving direction
+            dir_l,
+            dir_r,
+            dir_u,
+            dir_d,
 
+            # Food Location
+            game.food.x < game.head.x, # food left
+            game.food.x > game.head.x, # food right
+            game.food.y < game.head.y, # food up
+            game.food.y > game.head.y # food down (I think though it should be the other way round)
         ]
 
+        return np.array(state, dtype=int) # convert the list to a numpy array and turn the booleans to integers
+    
     def remember(self, state, action, reward, next_state, done):
-        pass
+        # we want to remember all of this in our memory
+        self.memory.append((state, action, reward, next_state, done)) # popleft if MAX_MEMORY is reached (Find out what popleft is)
 
     def train_long_memory(self):
-        pass
+        # here we are taking the variables from our memory (Specifically the no. of variables in a batch)
+        if len(self.memory) > BATCH_SIZE: 
+            mini_sample = random.sample(self.memory, BATCH_SIZE) # list of tuples 
+        else: # if we don't have enough elements in memory for a single batch
+            mini_sample = self.memory # then we jsut take the whole memory.
 
-    def train_short_memory(self, state, action, reward, next_state, done):
-        pass
+        states, actions, rewards, next_states, dones = zip(*mini_sample) # compiles into a list of tuples. not sure why there is an "*"
+        self.trainer.train_step(states, actions, rewards, next_states, dones) 
+
+    def train_short_memory(self, state, action, reward, next_state, done): 
+        self.trainer.train_step(state, action, reward, next_state, done) # all we need to train it for one game step but can take an array or tensor for batch size
+                                                                         
 
     def get_action(self, state):
-        pass
+        # random moves: tradeoff exploration / exploitation  (Random moves for exploration at the beginning)
+        self.epsilon = 80 - self.n_games # can play around with this. More games we have the smaller our epsilon and therefore number of random moves
+        final_move = [0, 0, 0]
+        if random.randint(0, 200) < self.epsilon: # random move generator
+            move = random.randint(0, 2)
+            final_move[move] = 1
+        else: # otherwise we are getting a prediction from our model.
+            pass
 
 def train():
     plot_scores = []
@@ -91,7 +121,7 @@ def train():
 
         if done:
             # train the long memory (replay memory), (experience replay) 
-            # so it trains agaon on all the previous moves and all the games it played (so super helpful)
+            # so it trains again on all the previous moves and all the games it played (so super helpful)
             # Also want to plot the result.
             game.reset()
             agent.n_games += 1
