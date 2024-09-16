94bedc4257940e4fe4644f37e9814198e2cebb47
Alexander Drysdale <drysdalealex0@gmail.com>
2024-09-16 13:27:21 +0100

    Runs and trains. Fix the dynamic plot

--- Machine Learning/PyTorch/Snake-pygame/agent.py
+++ Machine Learning/PyTorch/Snake-pygame/agent.py
@@ -3,20 +3,23 @@ import random
 import numpy as np
 from collections import deque # a data structure that is used to store our memeory
 from game import SnakeGameAI, Direction, Point
+from model import Linear_QNet, QTrainer
+from helper import plot
 
 MAX_MEMORY = 100_000
 BATCH_SIZE = 1000
 LR = 0.001
+
 
 class Agent:
 
     def __init__(self):
         self.n_games = 0
         self.epsilon = 0 # parameter to control the randomness 
-        self.gamma = 0 # discount rate 
+        self.gamma = 0.9 # discount rate, HAs to be smaller than one, is usually 0.8, 0.9.  
         self.memory = deque(maxlen=MAX_MEMORY) # if we excedd this memory
-        self.model = None 
-        self.trainer = None
+        self.model = Linear_QNet(11, 256, 3) # state array is input, no. of nodes in hidden layer, output_size (actions)
+        self.trainer = QTrainer(self.model, lr=LR, gamma=self.gamma)
         # TODO: model, trainer
     
     def get_state(self, game):
@@ -93,9 +96,11 @@ class Agent:
             final_move[move] = 1
         else: # otherwise we are getting a prediction from our model.
             state0 = torch.tensor(state, dtype=torch.float)
-            prediction = self.model.predict(state0)
+            prediction = self.model(state0)
             move = torch.argmax(prediction).item() # this is returning the index of the largest value in an array, which is then converted to a real number using .items()
             final_move[move] = 1
+        
+        return final_move
 
 def train():
     plot_scores = []
@@ -105,7 +110,7 @@ def train():
     agent = Agent()
     game = SnakeGameAI()
 
-    while True:
+    while True: # agent playing the game loop
         # get old state
         state_old = agent.get_state(game)
 
@@ -131,10 +136,15 @@ def train():
 
             if score > record:
                 record = score
-                # TODO agent.model.save()
+                agent.model.save()
 
                 print('Game', agent.n_games, 'Score', score, 'Record:', record)
-                # TODO: Plot
+                
+            plot_scores.append(score)
+            total_score += score
+            mean_score = total_score / agent.n_games
+            plot_mean_scores.append(mean_score)
+            plot(plot_scores, plot_mean_scores)
 
 
 if __name__ == '__main__':
