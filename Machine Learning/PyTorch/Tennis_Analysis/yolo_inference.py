8ca3c788823933c857fc024095d607866d240036
Alexander Drysdale <drysdalealex0@gmail.com>
2024-09-18 10:14:26 +0100

    Yolo model experiment

--- Machine Learning/PyTorch/Tennis_Analysis/yolo_inference.py
+++ Machine Learning/PyTorch/Tennis_Analysis/yolo_inference.py
@@ -1,1 +1,10 @@
 from ultralytics import YOLO
+
+model = YOLO('yolov8x') # x for extra large
+
+result = model.predict('Input_videos/image.png')
+
+print(result)
+print("boxes")
+for box in result[0].boxes:
+    print(box)
